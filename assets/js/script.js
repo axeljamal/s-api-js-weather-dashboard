e951eea094f3b4c5c90e09751d1067a4cc10b0ac
Axel Jamal <axeltaj@gmail.com>
2023-02-08 13:58:55 +0000

    declared vaiables; seacrhhButton, searchInput up until forecast, added event listener on searchButton when clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,24 @@
 const API_key = "5d654dc16d705cc36258a50daf547435";
+const searchButton = document.getElementById("search-button");
+const searchInput = document.getElementById("search-input");
+const todayWrapper = document.getElementById("today");
+let todayContent = document.createElement("div");
+const forecast = document.getElementById("forecast");
+
+
+
+// add event listener for search button input and to fetch the date
+searchButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    todayWrapper.innerHTML = "";
+    forecast.innerHTML = "";
+    clearItem();
+    let city = searchInput.value.trim();
+    if (city) {
+      getCity(city);
+      searchInput.value = "";
+      storeCity(city);
+    } else {
+      errorMsg();
+    }
+  });
