f0c223e6898d7d56e96966138e1b4d537e4a2f7f
Axel Jamal <axeltaj@gmail.com>
2023-02-09 13:41:49 +0000

    updates on script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,20 +5,18 @@ const todayWrapper = document.getElementById("today");
 let todayContent = document.createElement("div");
 const forecast = document.getElementById("forecast");
 
-
-
 // add event listener for search button input and to fetch the date
 searchButton.addEventListener("click", function (e) {
-    e.preventDefault();
-    todayWrapper.innerHTML = "";
-    forecast.innerHTML = "";
-    clearItem();
-    let city = searchInput.value.trim();
-    if (city) {
-      getCity(city);
-      searchInput.value = "";
-      storeCity(city);
-    } else {
-      errorMsg();
-    }
-  });
+  e.preventDefault();
+  todayWrapper.innerHTML = "";
+  forecast.innerHTML = "";
+  clearItem();
+  let city = searchInput.value.trim();
+  if (city) {
+    getCity(city);
+    searchInput.value = "";
+    storeCity(city);
+  } else {
+    errorMsg();
+  }
+});
